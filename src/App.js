cfc923acf46138ccad758659efc380f9113cf520
repti85 <ids-reptile@bk.ru>
2022-06-08 11:36:58 +0300

    add steps count, result declination

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-
 function App() {
     debugger
     const [value, setValue] = useState('');
-    const [randomNumberSTR, setRandomNumberSTR] = useState('');
-    let [bulls, setBulls] = useState(0);
-    let [cows, setCows] = useState(0);
+    const [randomNumberSTR, setRandomNumberSTR] = useState('');   
+    let [stepsCount, setStepsCount] = useState(0);
     const [steps, setSteps] = useState([]);
     const stepData = {
         stepCount: 0,
         userNumber: '',
         bulls: '0',
         cows: '0',
-    }
-    console.log(`bulls ${bulls}`);
-    console.log(`cows ${cows}`);
+    }   
 
     useEffect(() => {
         function randomNumber() {
             debugger
-            const arr = [0,1,2,3,4,5,6,7,8,9];
+            const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
             const newArr = [];
             let temp = 9;
             for (let i = 1; i < 5; i++) {
@@ -36,30 +32,20 @@ function App() {
             setRandomNumberSTR(newArr.join(''));
         }
         randomNumber()
-        
+
     }, [])
-    
-    // useEffect(() => randomNumber(), [])
-    
-    //const randomNumberSTR = randomNumber();
-    // randomNumber();
-    
+
     console.log(`randomNumber ${randomNumberSTR}`);
     let userNumber = value.toString();
     console.log(`userNumber ${userNumber}`);
-   stepData.userNumber = userNumber
+    stepData.userNumber = userNumber
 
-    
-    
-    // const userNumberSTR = userNumber.toString();
-    const reset = () => {
-        setBulls(0);
-        setCows(0);
-    }
-    function calc() {
+    const calc = () => {
         debugger;
-       let bullsCount = 0;
-       let cowsCount = 0;
+        // let stepCount = 0;
+        let bullsCount = 0;
+        let cowsCount = 0;
+
         for (let i = 0; i < randomNumberSTR.length; i++) {
 
             if (randomNumberSTR[i] === userNumber[i]) {
@@ -70,33 +56,48 @@ function App() {
             }
         }
         cowsCount = cowsCount - bullsCount;
+        setStepsCount(stepsCount + 1)
+        stepData.stepCount = stepsCount + 1;
         stepData.bulls = bullsCount;
         stepData.cows = cowsCount;
         setSteps(steps => steps.concat(stepData));
-        // setBulls(bullsCount);
-        // setCows(cowsCount);
-    
-
-        
     }
-    const result = steps.map(el => (<div>{el.userNumber} - {el.bulls} быков {el.cows} коров</div>))
-
-    
-    
-
+    const bullsDeclination = (el) => {
+        debugger
+        switch (el.bulls) {
+            case 0:
+                return 'Быков';
+            case 1:
+                return 'Бык';
+            case 2:
+            case 3:
+            case 4:
+                return 'Быка'
+        }
+    }
+    const cowsDeclination = (el) => {
+        debugger
+        switch (el.cows) {
+            case 0:
+                return 'Коров';
+            case 1:
+                return 'Корова';
+            case 2:
+            case 3:
+            case 4:
+                return 'Коровы'
+        }
+    }
+    const result = steps.map(el => (<div>{el.stepCount} - {el.userNumber} - {el.bulls} {bullsDeclination(el)} {el.cows} {cowsDeclination(el)}</div>))
     return (
         <div>
-            <textarea onChange={(event) => setValue(event.target.value)}/>
+            <textarea onChange={(event) => setValue(event.target.value)} />
             <button onClick={calc}>кнопка</button>
-            <button onClick={reset}>сброс</button>
-            <br/>
+            <br />
             <h3>{result}</h3>
-            {/* <h3>{bulls} Быка</h3>
-            <h3>{cows} Коровы</h3> */}
-
         </div>
     );
-    
+
 }
 export default App;
 
