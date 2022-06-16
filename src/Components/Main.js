ef288815f1d70162ebbc4881cd146d10414e6a8f
repti85 <ids-reptile@bk.ru>
2022-06-16 18:22:40 +0300

    refact

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,111 +1,9 @@
-import React, { createRef } from "react";
-import { useState, useEffect } from "react";
+import React from "react";
 import ResultList from "./ResultList";
 import InfoBar from "./InfoBar";
 import { Link } from "react-router-dom";
 
 function Main(props) {
-
-    const stepData = {
-        stepCount: 0,
-        userNumber: '',
-        bulls: 0,
-        cows: 0,
-        randomNumber: ''
-    }
-
-    const [value, setValue] = useState('');
-    const [randomNumberSTR, setRandomNumberSTR] = useState('');
-    const [stepsCount, setStepsCount] = useState(0);
-    const [steps, setSteps] = useState([]);
-    const [info, setInfo] = useState('')
-    const [disableBtnReset, setDisableBtnReset] = useState(true);
-    const [disableBtnShot, setDisableBtnShot] = useState(false);
-    const input = React.createRef()
-    const newGameBtn = React.createRef();
-
-
-    function randomNumber() {
-        const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        const newArr = [];
-        let temp = 9;
-        for (let i = 1; i < 5; i++) {
-            let random = Math.floor(Math.random() * temp);
-            newArr.push(arr[random])
-            arr.splice(random, 1);
-            temp -= 1;
-        }
-        setRandomNumberSTR(newArr.join(''));
-    }
-
-
-    useEffect(() => {
-        randomNumber();
-    }, [])
-
-
-
-    // console.log(`randomNumber ${randomNumberSTR}`);
-    let userNumber = value;
-    // console.log(`userNumber ${userNumber}`);
-    stepData.userNumber = userNumber
-
-    const check = () => {
-        // console.log(parseInt(userNumber))
-        if (isNaN(userNumber) === false && userNumber.length === 4) { //4-digit number check
-            if (new Set(userNumber).size !== userNumber.length) { //repeated digits check
-                setInfo('Цифры не должны повторяться')
-            } else {
-                setInfo('');
-                calc()
-            }
-        } else {
-            setInfo('Введите 4-значное число')
-        }
-    }
-
-    const calc = () => {
-        let bullsCount = 0;
-        let cowsCount = 0;
-
-        for (let i = 0; i < randomNumberSTR.length; i++) {
-            if (randomNumberSTR[i] === userNumber[i]) {
-                bullsCount += 1;
-            }
-            if (randomNumberSTR.indexOf(userNumber[i]) >= 0) {
-                cowsCount += 1;
-            }
-        }
-
-        if (bullsCount === 4) {
-            setInfo(`Вы отгадали число c ${(steps[0].stepCount) + 1}-й попытки :)`)
-            input.current.readOnly = true
-            setDisableBtnReset(false);
-            setDisableBtnShot(true);
-        }
-
-        cowsCount = cowsCount - bullsCount;
-        setStepsCount(stepsCount + 1)
-        stepData.stepCount = stepsCount + 1;
-        stepData.bulls = bullsCount;
-        stepData.cows = cowsCount;
-        stepData.randomNumber = randomNumberSTR;
-        setSteps(steps => [stepData, ...steps]);
-        setValue('');
-        input.current.focus();
-    }
-
-    const reset = () => {
-        setSteps([]);
-        setStepsCount(0);
-        setInfo('');
-        setValue('');
-        randomNumber();
-        input.current.focus();
-        input.current.readOnly = false
-        setDisableBtnShot(false);
-        setDisableBtnReset(true);
-    }
 
     return (
         <div>
@@ -115,17 +13,17 @@ function Main(props) {
             </div>
 
             <div className="form">
-                <input className="input" ref={input} autoFocus maxLength={4} value={value} onChange={(event) => setValue(event.target.value)} />
+                <input className="input" ref={props.input} autoFocus maxLength={4} value={props.value} onChange={(event) => props.setValue(event.target.value)} />
                 <div>
-                    <InfoBar info={info} />
+                    <InfoBar info={props.info} />
                 </div>
                 <div className="divButton">
-                    <button className="button" disabled={disableBtnShot} onClick={check}>Сделать ход</button>
-                    <button className="button" ref={newGameBtn} disabled={disableBtnReset} onClick={reset}>Новая игра</button>
+                    <button className="button" disabled={props.disableBtnShot} onClick={props.check}>Сделать ход</button>
+                    <button className="button" disabled={props.disableBtnReset} onClick={props.reset}>Новая игра</button>
                 </div>
             </div>
             <div className="resultBox">
-                <ResultList steps={steps} />
+                <ResultList steps={props.steps} />
             </div>
             <div className="logoBox">
                 <img src={props.logo} alt='bullLogo' className="logo" />
@@ -133,7 +31,6 @@ function Main(props) {
 
         </div>
     );
-
 }
 export default Main;
 
--- src/Components/ResultList.jsx
+++ src/Components/ResultList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../App.css';
+
 
 const ResultList = (props) => {
 const bullsDeclination = (el) => {
