04cb738154e091e0713030f1eccdfa47fe186d19
repti85 <ids-reptile@bk.ru>
2022-06-20 12:42:52 +0300

    rules correct

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import logo from './img/bull.png';
 import React from "react";
 import { useState, useEffect } from "react";
 
-const App = () => {
-
-    const rulesText = 'Компьютер задумывает четыре различные цифры из 0,1,2,...9. Игрок делает ходы, чтобы узнать эти цифры и их порядок. Каждый ход состоит из четырёх цифр, 0 может стоять на первом месте. В ответ компьютер показывает число отгаданных цифр, стоящих на своих местах (число быков) и число отгаданных цифр, стоящих не на своих местах (число коров).'
-    const rulesExample = 'Компьютер задумал 0834. Игрок сделал ход 8134. Компьютер ответил: 2 быка (цифры 3 и 4) и 1 корова (цифра 8).'
-
+const App = () => {    
 
     const stepData = {
         stepCount: 0,
@@ -26,8 +22,7 @@ const App = () => {
     const [info, setInfo] = useState('')
     const [disableBtnReset, setDisableBtnReset] = useState(true);
     const [disableBtnShot, setDisableBtnShot] = useState(false);
-    const [difficulty, setDifficulty] = useState(4)
-    const [radioChecked, setRadioChecked] = useState(true)
+    const [difficulty, setDifficulty] = useState(4)    
     const input = React.createRef()
 
 
@@ -151,7 +146,7 @@ const App = () => {
             </div>
             <div>
                 <Routes>
-                    <Route path="/rules" element={<Rules rulesText={rulesText} rulesExample={rulesExample} />} />
+                    <Route path="/rules" element={<Rules />} />
                     <Route path="/main" element={<GameArea getNumberDeclination={getNumberDeclination} addDifficultyRange={addDifficultyRange} difficulty={difficulty} input={input} logo={logo} value={value} setValue={setValue} info={info} disableBtnShot={disableBtnShot} checkInput={checkInput} disableBtnReset={disableBtnReset} resetGame={resetGame} steps={steps} />} />
                     <Route path="/*" element={<GameArea getNumberDeclination={getNumberDeclination} addDifficultyRange={addDifficultyRange} difficulty={difficulty} input={input} logo={logo} value={value} setValue={setValue} info={info} disableBtnShot={disableBtnShot} checkInput={checkInput} disableBtnReset={disableBtnReset} resetGame={resetGame} steps={steps} />} />
                 </Routes>
--- src/Components/Rules/Rules.jsx
+++ src/Components/Rules/Rules.jsx
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom"
 import "../../App.css"
 
 const Rules = (props) => {
+
+    const rulesText = 'Компьютер задумывает различные цифры из 0,1,2,...9 (количество зависит от выбранного уровня сложности). Игрок делает ходы, чтобы узнать эти цифры и их порядок, 0 может стоять на первом месте. В ответ компьютер показывает число отгаданных цифр, стоящих на своих местах (число быков) и число отгаданных цифр, стоящих не на своих местах (число коров).'
+    const rulesExample = 'Компьютер задумал 0834. Игрок сделал ход 8134. Компьютер ответил: 2 быка (цифры 3 и 4) и 1 корова (цифра 8).'
+
     return (
         <div>
             <div className="rules">
                 <h4>Правила игры</h4>
-                {props.rulesText}
+                {rulesText}
                 <h4>Пример</h4>
-                {props.rulesExample}
+                {rulesExample}
                 <div className="backLink">
                     <Link to='/main'><button className="button">Назад к игре</button></Link>
                 </div>
