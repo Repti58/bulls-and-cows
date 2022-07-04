0801d691313bc7b8474ee36a03c81289535ea40a
repti85 <ids-reptile@bk.ru>
2022-07-05 01:49:42 +0300

    2 tables work, output order problem

--- src/Components/GameHistory/GameHistory.js
+++ src/Components/GameHistory/GameHistory.js
@@ -3,62 +3,62 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const GameHistory = () => {
-    
+
     const [historyData, setHistoryData] = useState(null);
     const [bestResults, setBestResults] = useState(null);
 
 
-async function bestResultsFetch() {
-    debugger
-    let res = await fetch('http://localhost:3002/best_results')
-    res = await res.json()
-    console.log(`from bestResultsFetch ${res}`)
-    return res
-    // console.log(res);
-    // .then((res) => console.log(res.json()))
-    
-    
-    // .then((res) => console.log(res.json()))
-    // .then(res => setBestResults(res))    
-    // .catch(err => console.error(err));
-}
-
-async function historyDataFetch () {
-    debugger
-   let res = await fetch('http://localhost:3002/api')
-   res = await res.json()
-   return res
-//    console.log(res)
-    // .then((res) => console.log(res.json()))
-    // .then((res) => res.json())
-    // console.log(res.json())
-    // .then((res) => console.log(res.json()))
-    // .then(res => setHistoryData(res.reverse()))    
-    // .catch(err => console.error(err));
-}
-
-// async function fetchData() {
-// debugger
- 
-//     let response = await fetch('http://localhost:3002/api');
-//     let fetchHystoryData = await response.json();
-//     setHistoryData(fetchHystoryData)     
-    
-//     let response2 = await fetch('http://localhost:3002/api_best_results');
-//     let fetchBestResults = await response2.json();
-//     setBestResults(fetchBestResults)
-// }
+    async function bestResultsFetch() {
+        debugger
+        let res = await fetch('http://localhost:3002/best_results')
+        res = await res.json()
+        console.log(`from bestResultsFetch ${res}`)
+        return res
+        // console.log(res);
+        // .then((res) => console.log(res.json()))
 
 
-async function fetchRequests() {
-    debugger
-    const fetchHistoryData = await historyDataFetch();
-    console.log(fetchHistoryData);
-    setHistoryData(fetchHistoryData);
-    const fetchbestResults = await bestResultsFetch();
-    console.log(fetchbestResults);
-    setBestResults(fetchbestResults);
-}
+        // .then((res) => console.log(res.json()))
+        // .then(res => setBestResults(res))    
+        // .catch(err => console.error(err));
+    }
+
+    async function historyDataFetch() {
+        debugger
+        let res = await fetch('http://localhost:3002/api')
+        res = await res.json()
+        return res.reverse()
+        //    console.log(res)
+        // .then((res) => console.log(res.json()))
+        // .then((res) => res.json())
+        // console.log(res.json())
+        // .then((res) => console.log(res.json()))
+        // .then(res => setHistoryData(res.reverse()))    
+        // .catch(err => console.error(err));
+    }
+
+    // async function fetchData() {
+    // debugger
+
+    //     let response = await fetch('http://localhost:3002/api');
+    //     let fetchHystoryData = await response.json();
+    //     setHistoryData(fetchHystoryData)     
+
+    //     let response2 = await fetch('http://localhost:3002/api_best_results');
+    //     let fetchBestResults = await response2.json();
+    //     setBestResults(fetchBestResults)
+    // }
+
+
+    async function fetchRequests() {
+        debugger
+        const fetchHistoryData = await historyDataFetch();
+        console.log(fetchHistoryData);
+        setHistoryData(fetchHistoryData);
+        const fetchbestResults = await bestResultsFetch();
+        console.log(fetchbestResults);
+        setBestResults(fetchbestResults);
+    }
     useEffect(() => {
         debugger
         fetchRequests()
@@ -71,20 +71,20 @@ async function fetchRequests() {
         //     // .then((res) => console.log(res.json()))
         //     .then(res => setHistoryData(res.reverse()))
         //     .catch(err => console.error(err));
-            
-            // fetch('http://localhost:3002/api_best_results')
-            // // .then((res) => console.log(res.json()))
-            // .then((res) => res.json())
-            // // console.log(res.json())
-            // // .then((res) => console.log(res.json()))
-            // .then(res => setBestResults(res))
-            
-            // .catch(err => console.error(err));
-        }, [])
+
+        // fetch('http://localhost:3002/api_best_results')
+        // // .then((res) => console.log(res.json()))
+        // .then((res) => res.json())
+        // // console.log(res.json())
+        // // .then((res) => console.log(res.json()))
+        // .then(res => setBestResults(res))
+
+        // .catch(err => console.error(err));
+    }, [])
 
 
     function bestResultsTable() {
-        
+
         return (
             <table id="games">
                 <thead>
@@ -97,12 +97,12 @@ async function fetchRequests() {
                 </thead>
                 <tbody>
                     <tr>
-                    {/* <td>3</td>
+                        {/* <td>3</td>
                     <td>4</td>
                     <td>5</td> */}
-                    <td>{!bestResults ? "Loading" : bestResults[0].steps }</td>
-                    <td>{!bestResults ? "Loading" : bestResults[1].steps}</td>
-                    <td>{!bestResults ? "Loading" : bestResults[2].steps}</td>
+                        <td>{!bestResults ? "Loading" : bestResults[0].steps}</td>
+                        <td>{!bestResults ? "Loading" : bestResults[1].steps}</td>
+                        <td>{!bestResults ? "Loading" : bestResults[2].steps}</td>
                     </tr>
                 </tbody>
             </table>
@@ -110,11 +110,11 @@ async function fetchRequests() {
 
     }
     function GameHistoryTable() {
-        
+
         const table = () => {
             return (
-                <table id="games">
 
+                <table id="games">
                     <thead>
                         <tr>
                             <th>Дата</th>
@@ -152,7 +152,10 @@ async function fetchRequests() {
 
     return (
         <div>
+            <p className="title3">Лучшие результаты</p>
             <div>{bestResultsTable()}</div>
+            <p></p>
+            <p className="title3"> История Игр</p>
             <div>{GameHistoryTable()}</div>
         </div>
     )
