183b10f3e9c6210167c7fdcc3b879dc854a38982
repti85 <ids-reptile@bk.ru>
2022-08-14 19:19:08 +0300

    code align

--- src/Components/GameHistory/GameHistory.js
+++ src/Components/GameHistory/GameHistory.js
@@ -3,113 +3,120 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const GameHistory = () => {
+  const [historyData, setHistoryData] = useState(null);
+  const [bestResults, setBestResults] = useState(null);
 
-    const [historyData, setHistoryData] = useState(null);
-    const [bestResults, setBestResults] = useState(null);
+  async function bestResultsFetch() {
+    let res = await fetch(
+      "https://jade-glorious-fountain.glitch.me/best_results"
+    );
+    res = await res.json();
+    return res;
+  }
 
+  async function historyDataFetch() {
+    let res = await fetch("https://jade-glorious-fountain.glitch.me/api");
+    res = await res.json();
+    return res.reverse();
+  }
 
-    async function bestResultsFetch() {
-        
-        let res = await fetch('https://jade-glorious-fountain.glitch.me/best_results')
-        res = await res.json()        
-        return res
-    }
+  async function fetchRequests() {
+    const fetchHistoryData = await historyDataFetch();
+    console.log(fetchHistoryData);
+    setHistoryData(fetchHistoryData);
+    const fetchbestResults = await bestResultsFetch();
+    console.log(fetchbestResults);
+    setBestResults(fetchbestResults);
+  }
+  useEffect(() => {
+    fetchRequests();
+  }, []);
 
-    async function historyDataFetch() {
-        
-        let res = await fetch('https://jade-glorious-fountain.glitch.me/api')
-        res = await res.json()
-        return res.reverse()
-    }
-
-
-    async function fetchRequests() {
-        
-        const fetchHistoryData = await historyDataFetch();
-        console.log(fetchHistoryData);
-        setHistoryData(fetchHistoryData);
-        const fetchbestResults = await bestResultsFetch();
-        console.log(fetchbestResults);
-        setBestResults(fetchbestResults);
-    }
-    useEffect(() => {
-        
-        fetchRequests()
-    }, [])
-
-
-    function bestResultsTable() {
-
-        return (
-            <table id="games">
-                <thead>
-                    <tr>
-                        <th>3 цифры</th>
-                        <th>4 цифры</th>
-                        <th>5 цифр</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{!bestResults ? (<div className="loader"></div>) : bestResults[0].steps}</td>
-                        <td>{!bestResults ? (<div className="loader"></div>) : bestResults[1].steps}</td>
-                        <td>{!bestResults ? (<div className="loader"></div>) : bestResults[2].steps}</td>
-                    </tr>
-                </tbody>
-            </table>
-        )
-
-    }
-    function GameHistoryTable() {
-
-        const table = () => {
-            return (
-
-                <table id="games">
-                    <thead>
-                        <tr>
-                            <th>Дата</th>
-                            <th>Сложность</th>
-                            <th>Попытки</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {historyData.map((el, index) => {
-                            return (
-                                <tr key={index}>
-                                    <td>{el.date}</td>
-                                    <td>{el.difficulty}</td>
-                                    <td>{el.steps}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-
-                </table>
-            )
-        }
-
-        return (
-            <div>
-                <div>
-                    {!historyData ? (<div className="loader"></div>) : table()}
-                </div>
-                <div className="backLink">
-                    <Link to='/main' className='link'><button className="button">Назад к игре</button></Link>
-                </div>
-            </div>
-        )
-    }
+  function bestResultsTable() {
+    return (
+      <table id="games">
+        <thead>
+          <tr>
+            <th>3 цифры</th>
+            <th>4 цифры</th>
+            <th>5 цифр</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              {!bestResults ? (
+                <div className="loader"></div>
+              ) : (
+                bestResults[0].steps
+              )}
+            </td>
+            <td>
+              {!bestResults ? (
+                <div className="loader"></div>
+              ) : (
+                bestResults[1].steps
+              )}
+            </td>
+            <td>
+              {!bestResults ? (
+                <div className="loader"></div>
+              ) : (
+                bestResults[2].steps
+              )}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+  function GameHistoryTable() {
+    const table = () => {
+      return (
+        <table id="games">
+          <thead>
+            <tr>
+              <th>Дата</th>
+              <th>Сложность</th>
+              <th>Попытки</th>
+            </tr>
+          </thead>
+          <tbody>
+            {historyData.map((el, index) => {
+              return (
+                <tr key={index}>
+                  <td>{el.date}</td>
+                  <td>{el.difficulty}</td>
+                  <td>{el.steps}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      );
+    };
 
     return (
-        <div>
-            <p className="title3">Лучшие результаты</p>
-            <div>{bestResultsTable()}</div>
-            <p></p>
-            <p className="title3"> История игр</p>
-            <div>{GameHistoryTable()}</div>
+      <div>
+        <div>{!historyData ? <div className="loader"></div> : table()}</div>
+        <div className="backLink">
+          <Link to="/main" className="link">
+            <button className="button">Назад к игре</button>
+          </Link>
         </div>
-    )
-}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <p className="title3">Лучшие результаты</p>
+      <div>{bestResultsTable()}</div>
+      <p></p>
+      <p className="title3"> История игр</p>
+      <div>{GameHistoryTable()}</div>
+    </div>
+  );
+};
 
 export default GameHistory;
--- test.js
+++ test.js
@@ -1,20 +1,18 @@
-
 async function historyDataFetch() {
-    let res = (await fetch('http://localhost:3002/api')).json()
-    return res
+  let res = (await fetch("http://localhost:3002/api")).json();
+  return res;
 }
 
 async function bestResultsFetch() {
-    let res = (await fetch('http://localhost:3002/best_results')).json()
-    return res
+  let res = (await fetch("http://localhost:3002/best_results")).json();
+  return res;
 }
 
 async function fetchRequests() {
-
-    const fetchHistoryData = await historyDataFetch();
-    console.log(fetchHistoryData);
-    const fetchbestResults = await bestResultsFetch();
-    console.log(fetchbestResults);
+  const fetchHistoryData = await historyDataFetch();
+  console.log(fetchHistoryData);
+  const fetchbestResults = await bestResultsFetch();
+  console.log(fetchbestResults);
 }
 
-fetchRequests()
+fetchRequests();
