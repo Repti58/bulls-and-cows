a2f9c183d9a726a9be925f6bc86c3b78e351e5ee
repti85 <ids-reptile@bk.ru>
2023-09-07 20:18:21 +0300

    add config

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+	backendUrl: 'http://localhost:3005/api',
+	// backendUrl: "https://bulls-and-cows-backend.vercel.app/api"
+	// backendUrl: "https://wax-happy-sprint.glitch.me/api"
+}
+
+export default config
