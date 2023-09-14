b19d0a0681f59794901d931dde77549902638709
repti85 <ids-reptile@bk.ru>
2023-09-14 14:32:38 +0300

    correct config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-	backendUrl: 'http://localhost:3005',
-	// backendUrl: "https://bulls-and-cows-backend.vercel.app"
+	// backendUrl: 'http://localhost:3005',
+	backendUrl: "https://bulls-and-cows-backend.vercel.app"
 	// backendUrl: "https://wax-happy-sprint.glitch.me"
 }
 
